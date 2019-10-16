26f86f5f5fcf71624e64d85410cd486576109d95
Michael Nunes <miketnunes@gmail.com>
2019-10-15 22:01:14 -0700

    Add build for netlify deploy

---
+++ build/precache-manifest.571a5d6ad54a5a140569cca8e17c2ee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1f4356092e8359030b59b8005c28f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28f4e0e1e77ffbad03a3",
+    "url": "/static/css/main.8cf0f1c9.chunk.css"
+  },
+  {
+    "revision": "db7e498ea453daeb72c4",
+    "url": "/static/js/2.004e742c.chunk.js"
+  },
+  {
+    "revision": "28f4e0e1e77ffbad03a3",
+    "url": "/static/js/main.78274faa.chunk.js"
+  },
+  {
+    "revision": "7dfab99db576193d3036",
+    "url": "/static/js/runtime-main.735f3cbc.js"
+  }
+]);
